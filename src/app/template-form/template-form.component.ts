caf103227b386095e7018b3fa2c8ee15e6f935e3
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-04-27 11:41:00 -0300

    Adicionando Campos de endereço

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-template-form',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+  @ViewChild('nome') nomeValidado!: ElementRef;
+
+  onSubmit(form: any){
+    //console.log(form);
+    console.log(this.nomeValidado);
+    
+
+    //console.log(this.usuario);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
